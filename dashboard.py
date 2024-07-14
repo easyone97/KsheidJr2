2c8d92bf09ef9400a8f1df33199aaaabfb7a8452
easyone97 <112688368+easyone97@users.noreply.github.com>
2024-07-15 00:13:10 +0900

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -95,21 +95,21 @@ class DashboardApp:
             success_rate = success_cases / (success_cases + fail_cases) if (success_cases + fail_cases) > 0 else 0
 
             col1, col2, col3, col4 = st.columns(4,gap='large')
-               with col1:
-                    st.info('총 질문 수', icon="🔍")
-                    st.metric(label='', value=f"{total_cases}")
+            with col1:
+                st.info('총 질문 수', icon="🔍")
+                st.metric(label='', value=f"{total_cases}")
 
-                with col2:
-                    st.info('성공한 탈옥 질문 수', icon="🔍")
-                    st.metric(label='', value=f"{success_cases}")
+            with col2:
+                st.info('성공한 탈옥 질문 수', icon="🔍")
+                st.metric(label='', value=f"{success_cases}")
 
-                with col3:
-                    st.info('실패한 탈옥 질문 수', icon="🔍")
-                    st.metric(label='', value=f"{fail_cases}")
+            with col3:
+                st.info('실패한 탈옥 질문 수', icon="🔍")
+                st.metric(label='', value=f"{fail_cases}")
 
-                with col4:
-                    st.info('탈옥 성공률', icon="🔍")
-                    st.metric(label='', value=f"{success_rate:.2%}")
+            with col4:
+                st.info('탈옥 성공률', icon="🔍")
+                st.metric(label='', value=f"{success_rate:.2%}")
 
             st.markdown("<br><br>", unsafe_allow_html=True)
           
