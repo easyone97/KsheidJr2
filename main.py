360b4d2565f032f5c7bc9065c3b59348e40d1900
easyone97 <112688368+easyone97@users.noreply.github.com>
2024-07-15 02:14:59 +0900

    Update main.py

--- main.py
+++ main.py
@@ -63,6 +63,7 @@ if __name__ == '__main__':
             margin-top: 10px;
         }
         .footer-links a {
+            margin-left:20px;
             margin:10px;
             color: #25646F;
             font-weight: bold;
