beab2c961384369391bec8781f1d9a72a047d63c
easyone97 <112688368+easyone97@users.noreply.github.com>
2024-07-15 22:53:08 +0900

    Update prompt_history.py

--- prompt_history.py
+++ prompt_history.py
@@ -15,13 +15,6 @@ class PromptHistoryApp:
         self.parent_app = parent_app
 
     def run(self):
-        if 'results_df' not in st.session_state:
-            st.session_state.results_df = load_results('Downloadfile/final_result_test.csv')
-            st.session_state.filtered_df = st.session_state.results_df
-        if 'selected_types' not in st.session_state:
-            st.session_state.selected_types = ["전체"]
-        if 'selected_success' not in st.session_state:
-            st.session_state.selected_success = "전체"
         # 로딩 공간을 유지하기 위한 placeholder 생성
         placeholder = st.empty()
 
@@ -31,7 +24,9 @@ class PromptHistoryApp:
 
         # 로딩 화면 표시
         with st.spinner('로딩 중...'):
-            results_df = load_results('Downloadfile/final_result_test.csv')  # 파일 경로는 서버에 맞게 조정해야 합니다.
+            if 'results_df' not in st.session_state:
+                st.session_state.results_df = load_results('Downloadfile/final_result_test.csv')
+                st.session_state.filtered_df = st.session_state.results_df
 
         # 로딩 완료 후 placeholder 업데이트
         with placeholder.container():
@@ -63,8 +58,8 @@ class PromptHistoryApp:
                     font-weight: bold;
                     color: white;
                     margin-bottom: 10px;
-                    background-color: #3D9DF333;
-                    padding: 10px;
+                    background-color: #838383;
+                    padding: 5px;
                     border-radius: 5px;
                 }
                 .radio-label {
@@ -78,7 +73,6 @@ class PromptHistoryApp:
                     justify-content: flex-end;
                     margin-top: 10px;
                     margin-bottom: 10px;
-                    margin-left: 10px;
                 }
                 .spacer {
                     margin-bottom: 10px;
@@ -90,10 +84,10 @@ class PromptHistoryApp:
                 """,
                 unsafe_allow_html=True
             )
-            st.markdown("<h1 style='font-size: 2.5em; color: #FFFFFF;'>탈옥 프롬프트 내역</h1>", unsafe_allow_html=True)
+            st.markdown("<h1 style='font-size: 2.5em; color: #000000;'>탈옥 프롬프트 내역</h1>", unsafe_allow_html=True)
             st.markdown("<br><br>", unsafe_allow_html=True)
 
-            if results_df.empty:
+            if st.session_state.results_df.empty:
                 st.warning("No data available to display.")
                 return
 
@@ -111,7 +105,15 @@ class PromptHistoryApp:
                     st.markdown("<div class='filter-label'>Type 선택</div>", unsafe_allow_html=True)
                     type_options = ["전체"] + st.session_state.results_df['type'].unique().tolist()
                     selected_types = st.multiselect("", type_options, default=st.session_state.selected_types)
-                    st.session_state.selected_types = selected_types
+                    st.markdown("<div class='apply-button'>", unsafe_allow_html=True)
+                    if st.button("적용", key="apply_button"):
+                        st.session_state.selected_types = selected_types
+                        st.session_state.filtered_df = st.session_state.results_df.copy()
+                        if "전체" not in st.session_state.selected_types:
+                            st.session_state.filtered_df = st.session_state.filtered_df[st.session_state.filtered_df['type'].isin(st.session_state.selected_types)]
+                        if st.session_state.selected_success != "전체":
+                            st.session_state.filtered_df = st.session_state.filtered_df[st.session_state.filtered_df['탈옥성공여부'] == st.session_state.selected_success]
+                    st.markdown("</div>", unsafe_allow_html=True)
 
                 st.markdown("<div class='container-spacing'></div>", unsafe_allow_html=True)
 
@@ -123,16 +125,6 @@ class PromptHistoryApp:
                     selected_success_actual = success_options_actual[success_options_display.index(selected_success_display)]
                     st.session_state.selected_success = selected_success_actual
                     st.markdown('<style>.stRadio > div {display: flex; flex-direction: column;}</style>', unsafe_allow_html=True)
-
-                st.markdown("<div class='apply-button'>", unsafe_allow_html=True)
-                if st.button("적용", key="apply_button"):
-                    # 선택된 필터에 따라 데이터 필터링
-                    st.session_state.filtered_df = st.session_state.results_df.copy()
-                    if "전체" not in st.session_state.selected_types:
-                        st.session_state.filtered_df = st.session_state.filtered_df[st.session_state.filtered_df['type'].isin(st.session_state.selected_types)]
-                    if st.session_state.selected_success != "전체":
-                        st.session_state.filtered_df = st.session_state.filtered_df[st.session_state.filtered_df['탈옥성공여부'] == st.session_state.selected_success]
-                st.markdown("</div>", unsafe_allow_html=True)
 
             # 필터링된 데이터 표시
             if st.session_state.filtered_df is not None:
@@ -164,6 +156,7 @@ class PromptHistoryApp:
 if __name__ == "__main__":
     app = PromptHistoryApp()
     app.run()
+
 
 
 
